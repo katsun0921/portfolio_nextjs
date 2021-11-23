2c02aa41746ee7d07ef87e38de867977056057dc
Katsumasa-Sato <katsumasa-sato@outlook.com>
2021-11-23 15:59:12 +0900

    Add tailwindcss to package

---
+++ pages/blog-page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import React from 'react'
+
+const blogPage: NextPage = () => {
+    return (
+        <div>
+            Blog Page
+        </div>
+    )
+}
+
+export default blogPage
+
---
+++ pages/contact-page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import React from 'react'
+
+const contactPage: NextPage = () => {
+    return (
+        <div>
+            Contact Page
+        </div>
+    )
+}
+
+export default contactPage
+

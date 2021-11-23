37af1caa21cb13cc669bd6e7938954aaceb9cb43
Katsumasa-Sato <katsumasa-sato@outlook.com>
2021-11-23 15:23:27 +0900

    Create Nuxt Js and npm install tailwindcss

---
+++ pages/blog-page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <div>
+            Blog Page
+        </div>
+    )
+}
+
+export default blogPage
+
---
+++ pages/contact-page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const contactPage = () => {
+    return (
+        <div>
+            Contact Page
+        </div>
+    )
+}
+
+export default contactPage
+

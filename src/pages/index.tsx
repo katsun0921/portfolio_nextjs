b4a64e7dce9a7c12038a7c61df7380843bb7a464
Katsumasa-Sato <katsumasa-sato@outlook.com>
2022-02-04 09:13:05 +0900

    Create pc layout to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,18 +9,18 @@ const Home: NextPage = (): ReactElement => {
   return (
     <Layout title={pageTitle.home} page={pageName.home}>
       <div
-        className={`${homeStyles.name__block} ${homeStyles.block} top-1/2 left-1/2`}
+        className={`${homeStyles.name__block} ${homeStyles.block} w-11/12 mt-11 lg:m-0 lg:top-1/2 lg:left-1/2`}
       >
         <div
           className={`${homeStyles.container} ${homeStyles.name__container}`}
         >
-          <h1 className={`${homeStyles.name__text} drop-shadow`}>
+          <h1 className={`${homeStyles.name__text} drop-shadow text-4xl`}>
             KATSUMASA SATO
-            <span className={`${homeStyles.name__text__sub} block`}>
+            <span className={`${homeStyles.name__text__sub} block text-2xl`}>
               WEB DEVELOPER
             </span>
           </h1>
-          <ul className="mt-5">
+          <ul className="mt-1 lg:mt-5">
             <li className={`${homeStyles.name__linkList}`}>
               Twitter:
               <Link href="https://twitter.com/Katsun0921/" passHref>
@@ -56,8 +56,8 @@ const Home: NextPage = (): ReactElement => {
           </ul>
         </div>
       </div>
-      <ul className={`${homeStyles.contents} absolute top-1/2 left-1/2`}>
-        <li>
+      <ul className={`w-11/12 mt-3 lg:absolute lg:top-1/2 lg:left-1/2`}>
+        <li className={`${homeStyles.content__list} border-t`}>
           <Link href="/profile">
             <a
               className={`${homeStyles.block} ${homeStyles.content__block} ${homeStyles.profile__block}`}
@@ -74,7 +74,7 @@ const Home: NextPage = (): ReactElement => {
             </a>
           </Link>
         </li>
-        <li>
+        <li className={`${homeStyles.content__list}`}>
           <Link href="/work-express">
             <a
               className={`${homeStyles.block} ${homeStyles.content__block} ${homeStyles.workExpress__block}`}
@@ -89,7 +89,7 @@ const Home: NextPage = (): ReactElement => {
             </a>
           </Link>
         </li>
-        <li>
+        <li className={`${homeStyles.content__list}`}>
           <Link href="/blog">
             <a
               className={`${homeStyles.block} ${homeStyles.content__block} ${homeStyles.blog__block}`}
